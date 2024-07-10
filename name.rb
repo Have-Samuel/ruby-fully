bf7dc84fc8ac277f73af6e475f72610ded7913ee
samuel <samiehave9@gmail.com>
2024-07-10 16:11:39 +0300

    Create name.rb and a name program

---
+++ name.rb
@@ -0,0 +1,12 @@
+name = 'Whatsapp?'
+
+def type_your_name(name)
+    puts "What is your name?"
+    name = gets.chomp
+end
+type_your_name(name)
+puts name
+
+puts "What is your name?"
+name = gets.chomp
+puts "Helo #{name}"
