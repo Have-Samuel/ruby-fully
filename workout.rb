df5f5fbbb1436fd1a124295233397e1cbc90d3f6
samuel <samiehave9@gmail.com>
2024-07-10 12:04:05 +0300

    Testings

---
+++ workout.rb
@@ -0,0 +1,11 @@
+name = 'Intro to Ruby'
+
+def print_full_name(first_name, last_name)
+  name = first_name + ' ' + last_name
+  puts name
+end
+
+print_full_name('Have', 'Samuel')
+print_full_name('Longlive', 'Samson')
+print_full_name('Kabareebe', 'Samuel')
+puts name
