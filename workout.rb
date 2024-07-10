5c3f888d69a6867a847c97ae7252d06978bc064e
samuel <samiehave9@gmail.com>
2024-07-10 12:41:51 +0300

    Testing

--- workout.rb
+++ workout.rb
@@ -9,3 +9,14 @@ print_full_name('Have', 'Samuel')
 print_full_name('Longlive', 'Samson')
 print_full_name('Kabareebe', 'Samuel')
 puts name
+
+# Variable Scope and Blocks
+total = 0
+[1, 2, 3].each { |number| total += number }
+puts total # 6
+
+total = 0
+[1,2,3,4].each do |number|
+    total = number + number
+end
+puts total # 8
