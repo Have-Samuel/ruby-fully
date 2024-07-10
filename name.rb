f3d86f692214356a7e1f4be46aa1412eb0a02424
samuel <samiehave9@gmail.com>
2024-07-10 17:34:20 +0300

    Scopes and Blocks

--- name.rb
+++ name.rb
@@ -28,5 +28,18 @@ last_name = gets.chomp
 
 puts "So your names are #{first_name} #{last_name}"
 
+# Look at the following programs...
+# x = 0
+# 3.times do
+#     x += 1
+# end
+# puts x
 
+y = 0
+3.times do
+    y += 1
+    x = y
+end
+puts x
 
+# What does x print to the screen in each case? Do they both give errors? Are the errors different? Why?
